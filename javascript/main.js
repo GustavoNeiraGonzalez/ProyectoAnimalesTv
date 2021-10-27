c7412e571b9b13f6043bf990a73f7d1db6324c33
GustavoNeira <gustavoeduardoneiragonzalez@gmail.com>
2021-10-27 15:10:22 -0300

    arreglo menor noticias css, funcion cerrar menu en javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,14 @@
-function QuitarCheckbox(){
-    if (document.getElementById("menu-bar").checked = true){
-        document.write(5+6);
-    }
+'use strict'
+        /*Este codigo hará que el nav desaparezca cuando se haga click fuera de el, osea solo en lo que queda abajo del nav */
+        function CambiarCheckbox(){
+                if(document.getElementById("menu-bar").checked == true){
+                        document.getElementById("menu-bar").checked = false;
+                }   
+        }
+        function clickBody(){
+                document.querySelector("main").addEventListener("click",CambiarCheckbox);
 
-}
+        }
+        
+
+
