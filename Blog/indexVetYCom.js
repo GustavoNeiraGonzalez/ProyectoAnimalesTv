c505d1a0778903575d83e7af5a8aab6e0b829a6f
GustavoNeira <gustavoeduardoneiragonzalez@gmail.com>
2021-11-29 17:57:32 -0300

    cambios menores, debo ver como ejecutar archivo pug en el servidor

--- Blog/indexVetYCom.js
+++ Blog/indexVetYCom.js
@@ -21,9 +21,7 @@ const vetYCom = [
         1:[{
             linkIcono: 'https://static.wixstatic.com/media/4ef24d_783c41e58d334e00976c9c4b7739604d~mv2.jpg/v1/fill/w_149,h_149,al_c,q_80,usm_0.66_1.00_0.01/4ef24d_783c41e58d334e00976c9c4b7739604d~mv2.webp',
             whatsapp:'22 815 55 88',
-            texoNegrita:['Nuestros horarios de atención'],
-            textoNormal:['lunes a sábado de 10:00-15:00 y 16:00-18:00','(Colación de 15:00-16:00)'],
-            textoSubrayado:['este texto esta subrayado'],
+            texto:['*Nuestros horarios de atención','%este texto esta subrayado','lunes a sábado de 10:00-15:00 y 16:00-18:00','(Colación de 15:00-16:00)'],
             linkRedireccionar:['http://www.integravet.cl/'],
             imagenInstagram:['https://static.wixstatic.com/media/4ef24d_6138ae673c0b46b3b69ec4e15215468c~mv2.png/v1/fill/w_144,h_47,al_c,q_85,usm_0.66_1.00_0.01/4ef24d_6138ae673c0b46b3b69ec4e15215468c~mv2.webp'],
             imagenFacebook:['https://static.wixstatic.com/media/4ef24d_7f8fd7c2c2364d74bf6628081bcaa27c~mv2.png/v1/fill/w_168,h_27,al_c,q_85,usm_0.66_1.00_0.01/4ef24d_7f8fd7c2c2364d74bf6628081bcaa27c~mv2.webp']
@@ -35,9 +33,7 @@ const vetYCom = [
         1:[{
             linkIcono: 'https://static.wixstatic.com/media/4ef24d_783c41e58d334e00976c9c4b7739604d~mv2.jpg/v1/fill/w_149,h_149,al_c,q_80,usm_0.66_1.00_0.01/4ef24d_783c41e58d334e00976c9c4b7739604d~mv2.webp',
             whatsapp:'22 815 55 88',
-            texoNegrita:['Nuestros horarios de atención'],
-            textoNormal:['lunes a sábado de 10:00-15:00 y 16:00-18:00','(Colación de 15:00-16:00)'],
-            textoSubrayado:['este texto esta subrayado'],
+            texto:['*Nuestros horarios de atención','%este texto esta subrayado','lunes a sábado de 10:00-15:00 y 16:00-18:00','(Colación de 15:00-16:00)'],
             linkRedireccionar:['http://www.integravet.cl/'],
             imagenInstagram:['https://static.wixstatic.com/media/4ef24d_6138ae673c0b46b3b69ec4e15215468c~mv2.png/v1/fill/w_144,h_47,al_c,q_85,usm_0.66_1.00_0.01/4ef24d_6138ae673c0b46b3b69ec4e15215468c~mv2.webp'],
             imagenFacebook:['https://static.wixstatic.com/media/4ef24d_7f8fd7c2c2364d74bf6628081bcaa27c~mv2.png/v1/fill/w_168,h_27,al_c,q_85,usm_0.66_1.00_0.01/4ef24d_7f8fd7c2c2364d74bf6628081bcaa27c~mv2.webp']
@@ -65,9 +61,7 @@ app.get('/posts/:i/:handle',(req, res) =>{ // Dos puntos en la URL creará una v
     res.render(`vetYCom`, { // Para usar la variable escribir req.params.NOMBRE_VARIABLE
         linkIcono: vetYCom[req.params.i].linkIcono,
         whatsapp: vetYCom[req.params.i].whatsapp,
-        texoNegrita: vetYCom[req.params.i].texoNegrita,
-        textoNormal: vetYCom[req.params.i].textoNormal,
-        textoSubrayado: vetYCom[req.params.i].textoSubrayado,
+        texo: vetYCom[req.params.i].texo,
         linkRedireccionar: vetYCom[req.params.i].linkRedireccionar,
         imagenInstagram: vetYCom[req.params.i].imagenInstagram,
         imagenFacebook: vetYCom[req.params.i].imagenFacebook
